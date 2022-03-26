24032a25916c3d44d619dc1c8085affc4ea7bf98
xiao_chang <2556940481@qq.com>
2022-03-27 01:17:39 +0800

    chore:  项目hexo 备份

--- js/index.js
+++
@@ -1,7 +0,0 @@
-// tab 激活状态
-$(function () {
-  $("#myMenu li").click(function () {
-    $(this).children("a").addClass("tab_active");
-    $(this).siblings().children("a").removeClass("tab_active");
-  });
-});
---
+++ source/categories/index.md
@@ -0,0 +1,5 @@
+---
+title: 分类
+date: 2022-03-26 01:16:22
+type: "categories"
+---
